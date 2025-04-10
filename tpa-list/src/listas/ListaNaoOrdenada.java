3075443c82f35bed8e760420a3e379ebe1273074
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-04-10 20:58:31 -0300

    Implementa método pesquisar em LNO

--- tpa-list/src/listas/ListaNaoOrdenada.java
+++ tpa-list/src/listas/ListaNaoOrdenada.java
@@ -24,4 +24,15 @@ public class ListaNaoOrdenada<T> {
         this.qtd++;
     }
 
+    public T pesquisar(T valor){
+        No<T> aux = this.prim;
+
+        while(aux != null){
+            if(valor.equals(aux.getValor())){
+                return aux.getValor();
+            }
+            aux = aux.getProx();
+        }
+        return null;
+    }
 }
