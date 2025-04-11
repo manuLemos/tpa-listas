22e4a39e212c86dded4e9970b8c1920be40e14cc
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-04-10 21:38:06 -0300

    Implementa métodos e atributos da classe aluno

--- tpa-list/src/teste/Aluno.java
+++ tpa-list/src/teste/Aluno.java
@@ -1,4 +1,33 @@
 package teste;
 
 public class Aluno {
+    private String nome;
+    private int matricula;
+    private double nota;
+
+    public Aluno(String nome, int matricula, double nota) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
 }
