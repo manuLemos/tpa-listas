a9ffbc5c0c673a0f049c8eea71c59f22eb45df4f
Guilherme Gomes <guilhermeneto26@hotmail.com>
2025-04-10 21:39:27 -0300

    Implementa comparador classe aluno
    Compara valores de matrícula da classe Aluno

--- tpa-list/src/teste/Comparador.java
+++ tpa-list/src/teste/Comparador.java
@@ -1,4 +1,10 @@
 package teste;
 
-public class Comparador {
+import java.util.Comparator;
+
+public class Comparador implements Comparator<Aluno> {
+    @Override
+    public int compare(Aluno a1, Aluno a2) {
+        return Integer.compare(a1.getMatricula(), a2.getMatricula());
+    }
 }
