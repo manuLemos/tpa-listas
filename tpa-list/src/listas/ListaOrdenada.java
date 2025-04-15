b8084e21752e8020a12c0a7840dc72ab508d6b31
Manuely Meireles Lemos <manumeireleslemos@gmail.com>
2025-04-15 10:36:01 -0300

    Implementa método adicionar elemento

--- tpa-list/src/listas/ListaOrdenada.java
+++ tpa-list/src/listas/ListaOrdenada.java
@@ -13,4 +13,35 @@ public class ListaOrdenada <T> {
         this.ult = null;
         this.comparador = comparador;
     }
+
+    public void adicionar(T novoValor) {
+        No<T> novo = new No<>(novoValor);
+        No<T> atual,ant;
+        atual = this.prim;
+        ant = null;
+
+        if (this.prim == null) {
+            this.prim = novo;
+            this.ult = novo;
+        }
+        else {
+            while (atual != null && comparador.compare(atual.getValor(), novoValor) < 0) {
+                ant=atual;
+                atual=atual.getProx();
+            }
+            if(ant==null) {
+                novo.setProx(this.prim);
+                this.prim=novo;
+            }
+            else if(atual==null){
+                this.ult.setProx(novo);
+                this.ult=novo;
+            }
+            else {
+                ant.setProx(novo);
+                novo.setProx(this.ult);
+            }
+        }
+        this.qtd++;
+    }
 }
