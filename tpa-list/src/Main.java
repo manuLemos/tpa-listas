dd6f2e89e33f9b1102d6410f2789d7ebf758c04c
Manuely Meireles Lemos <manumeireleslemos@gmail.com>
2025-04-15 10:37:36 -0300

    testes

--- tpa-list/src/Main.java
+++ tpa-list/src/Main.java
@@ -1,5 +1,21 @@
+import listas.ListaNaoOrdenada;
+import listas.ListaOrdenada;
+import teste.Comparador;
+import teste.Aluno;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Aluno a1 = new Aluno("MANU",23,10);
+        Aluno a2 = new Aluno("MARIA",123,8);
+        Aluno a3 = new Aluno("MAISA",1,9);
+
+        Comparador comparador = new Comparador();
+
+        ListaOrdenada<Aluno> alunos = new ListaOrdenada<>(comparador);
+        alunos.adicionar(a1);
+        alunos.adicionar(a2);
+        alunos.adicionar(a3);
+
+        System.out.println(alunos);
     }
 }
